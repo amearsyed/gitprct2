b4768e6686c2302f73f3a8ebca7b4b3d7d22157f
amearsyed <amearsyed6@gmail.com>
2022-09-14 14:54:29 +0530

    huu

---
+++ src/test/java/Akash/Demo8.java
@@ -0,0 +1,9 @@
+package Akash;
+
+public class Demo8 {
+
+	public static void main(String[] args) {
+		System.out.println("hi akash");
+	}
+
+}
---
+++ src/test/java/Akash/Demo9.java
@@ -0,0 +1,9 @@
+package Akash;
+
+public class Demo9 {
+
+	public static void main(String[] args) {
+		System.out.println("hi ak1");
+	}
+
+}
