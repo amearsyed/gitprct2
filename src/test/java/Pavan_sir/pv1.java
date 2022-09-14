e371d2c08b8595e00db0b97f5fe94d958940e1db
amearsyed <amearsyed6@gmail.com>
2022-09-14 15:24:06 +0530

    jj

---
+++ src/test/java/Pavan_sir/pv1.java
@@ -0,0 +1,9 @@
+package Pavan_sir;
+
+public class pv1 {
+
+	public static void main(String[] args) {
+		System.out.println("hi sir");
+	}
+
+}
---
+++ src/test/java/lata/Demo9.java
@@ -0,0 +1,10 @@
+package lata;
+
+public class Demo9 {
+
+	public static void main(String[] args) {
+		System.out.println("hi akash");
+		System.out.println("hello");
+	}
+
+}
